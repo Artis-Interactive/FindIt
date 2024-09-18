971a8ec0cbf4886db01b8fa20d93902e87eb16bf
dannycg25 <dannycg25@gmail.com>
2024-09-18 13:55:43 -0600

    Implementation of the hero image container for the company page.

--- findit-frontend/src/CompanyPage.vue
+++ findit-frontend/src/CompanyPage.vue
@@ -4,16 +4,29 @@
       <AppHeader />
     </Header>
   </div>
+  <div class="hero">
+    <div class="image-container">
+      <img :src="heroImage" alt="Hero Image" class="hero-image">
+    </div>
+  </div>
 </template>
   
 <script>
 import AppHeader from './components/AppHeader.vue'
+
 export default {
     name: 'CompanyPage',
     components: {
       AppHeader
+    },
+    data() {
+    return {
+      heroImage: require("./assets/company_page_hero.jpg"),
+      companyName: "N&W Jewerly",
+      companySchedule: "Horario: 10:00 AM - 5:00PM",
     }
   }
+}
 </script>
 
 <style>
@@ -22,5 +35,17 @@ export default {
         padding: 0;
         width: 100%;
         height: 100%;
+    }
+
+    .image-container {
+        width: 2505px;
+        height: 500px;
+        overflow: hidden;
+    }
+
+    .hero-image {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
     }
 </style>

cca16ef5713cc5977eefa1bc57c9ced030f1f1b8
DariALV <darialvjs@gmail.com>
2024-09-20 10:54:11 -0600

    HTML and CSS code for the Product Details Page

---
+++ findit-frontend/src/components/ProductDetails.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="product-container">
+      <div id="regresar">
+          Volver atras
+      </div>
+      <div id="img-container">
+          <img src="../assets/logo.png" alt="El logo de Vue">
+      </div>
+      <div id="description-container">
+          Descripcion
+      </div>
+      <div id="buy-container">
+          Comprar
+      </div>
+  </div>
+</template>
+
+<script>
+  export default {
+      name: 'ProductViewBody'
+  }
+</script>
+
+<style scoped>
+
+  #product-container {
+      margin: 0px;
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-template-rows: 50px 600px;
+  }
+
+  #regresar {
+      background-color: aqua;
+      grid-column: 1 / 4;
+  }
+
+  #img-container {
+      background-color: greenyellow;
+  }
+
+  #description-container {
+      background-color: yellow;
+  }
+
+  #buy-container {
+      background-color: red;
+  }
+
+</style>

d8fb567eb7c4ccd5e4c5477fbb87313023438a9b
Sebastián Bolaños <sebasbose@gmail.com>
2024-09-22 18:16:15 -0600

    Added tailwind to the vue.js frontend project.

--- findit-frontend/src/main.js
+++ findit-frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './index.css'
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HelloWorld from './components/HelloWorld.vue'

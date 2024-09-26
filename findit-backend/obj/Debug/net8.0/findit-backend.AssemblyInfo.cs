f94056dc2cafbd57a8c7589d35b903202f8a1b0f
RodriguezValeria <valeria.rodriguez21@ucr.ac.cr>
2024-09-26 11:05:54 -0600

    Missing documents to add

--- findit-backend/obj/Debug/net8.0/findit-backend.AssemblyInfo.cs
+++ findit-backend/obj/Debug/net8.0/findit-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("findit-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c2b7102e71128522e9140775c4fb48d967c7ef1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f37e31e22f614be7a12947d7548add8ab3af297")]
 [assembly: System.Reflection.AssemblyProductAttribute("findit-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("findit-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

318a43c1e75db1008d4d7f1511c247ad0e997885
DariALV <darialvjs@gmail.com>
2024-09-27 08:07:29 -0600

    Front-end design for My Profile Page almost done.

---
+++ findit-frontend/src/components/MyProfile/MyProfile.vue
@@ -0,0 +1,87 @@
+<template>
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
+  <div class="general-container">
+    <div class="user-container">
+      <img src="../assets/image.png" alt="Imagen de usuario">
+      <h2>Jose Perez</h2>
+    </div>
+    <div class="btns">
+      <button class="personal-data btn"><i class="uil uil-user"></i>Información personal</button>
+      <button class="payment-methods btn"><i class="uil uil-credit-card"></i>Métodos de pago</button>
+      <button class="locations btn"><i class="uil uil-location-point"></i>Direcciones</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;800&display=swap');
+  
+  .general-container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .user-container{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 40px;
+    width: 80%;
+    min-width: 800px;
+  }
+
+  .user-container img {
+    width: 200px;
+    border-radius: 100%;
+    margin-right: 20px;
+  }
+
+  .user-container h2{
+    font-family: 'poppins', sans-serif;
+    font-weight: 600;
+    font-size: 2.5em;
+  }
+
+  .btns{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    background-color: #E3DDEC;
+    width: 80%;
+    min-width: 800px;
+    height: 100%;
+    min-height: 450px;
+    align-items: center;
+    justify-items: center;
+    justify-self: center;
+    align-self: center;
+    border-radius: 10px;
+  }
+  
+  .btns button {
+    border: none;
+    outline: none;
+    margin: 5px 0;
+    padding: 0.6vw 0;
+    width: 70%;
+    height: 70%;
+    font-size: 1.5em;
+    cursor: pointer;
+    background-color: #8263A8;
+    color: white;
+    font-family: 'poppins', sans-serif;
+    font-weight: 600;
+    transition: 0.2s ease;
+    margin-left: 10px;
+  }
+
+  .btns button:hover{
+    scale: 1.05;
+  }
+</style>

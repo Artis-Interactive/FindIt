5da3acf8fe97133ae00c5fe16100268ed7ac22c1
FelipeMena11 <felipe.menarodriguez@ucr.ac.cr>
2024-09-28 18:19:54 -0600

    Unified User Controllers and fixed token generation to include claims

--- findit-backend/Controllers/UserController.cs
+++ findit-backend/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 using findit_backend.Handlers;
 using findit_backend.Models;
@@ -11,27 +12,33 @@ namespace findit_backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UsersController : ControllerBase
+    public class UserController : ControllerBase
     {
-        private readonly UsersHandler _usersHandler;
+        private readonly UserHandler _userHandler;
         private IConfiguration _config;
 
-        public UsersController(IConfiguration config)
+        public UserController(IConfiguration config)
         {
-            _usersHandler = new UsersHandler();
+            _userHandler = new UserHandler();
             _config = config;
         }
         // Generate user authentication token:
-        private string GenerateJSONWebToken()
+        private async Task<string> GenerateJSONWebToken(UserLoginModel user)
         {
             // Obtains key from appsettings as a byte array:
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
             // Defines signing method for token based on key and HMAC with SHA-256 encryption algorithm:
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var userRole = await _userHandler.getUserRole(user);
+            var claims = new[]
+            {
+                new Claim("email", user.Email),
+                new Claim("role", userRole),
+            };
             // create token:
             var token = new JwtSecurityToken(issuer: _config["Jwt:Issuer"],
                 audience: _config["Jwt:Issuer"],
-                claims: null,
+                claims: claims,
                 expires: null,
                 signingCredentials: credentials);
 
@@ -47,11 +54,11 @@ namespace findit_backend.Controllers
                 {
                     return BadRequest("User data is null");
                 }
-                string result = await _usersHandler.validateUser(user);
+                string result = await _userHandler.validateUser(user);
 
                 if (result == "Allow")
                 {
-                    var token = GenerateJSONWebToken();
+                    var token = await GenerateJSONWebToken(user);
                     return Ok(new { token, message = result });
                 }
                 else

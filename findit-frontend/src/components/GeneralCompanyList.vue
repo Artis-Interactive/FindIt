b55098bb5e45cea49da5302fa5e143ede03c7021
dannycg25 <dannycg25@gmail.com>
2024-10-23 00:23:31 -0600

    General company list to grid

--- findit-frontend/src/components/GeneralCompanyList.vue
+++ findit-frontend/src/components/GeneralCompanyList.vue
@@ -8,142 +8,158 @@
     <div class="row justify-content-center">
       <h1 class="title">Empresas Registradas</h1>
     </div>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Nombre</th>
-          <th>Identificación</th>
-          <th>Número de teléfono</th>
-          <th>Correo</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(company, index) in companies" :key="index">
-          <td>{{ company.name }}</td>
-          <td>{{ company.legalID }}</td>
-          <td>{{ company.phoneNumber }}</td>
-          <td>{{ company.email }}</td>
-        </tr>
-      </tbody>
-    </table>
-    <ModalComponent
-        :isVisible="isModalVisible"
-        :title="modalTitle"
-        @close="isModalVisible = false"
-      >
-        <template #body>
-          <p>{{ modalMessage }}</p>
-        </template>
-      </ModalComponent>
+    <div ref="grid"></div>  
   </div>
+  <ModalComponent
+    :isVisible="isModalVisible"
+    :title="modalTitle"
+    @close="isModalVisible = false"
+  >
+    <template #body>
+      <p>{{ modalMessage }}</p>
+    </template>
+  </ModalComponent>
 </template>
 
 <script>
-import axios from "axios";
-import ModalComponent from "./ModalComponent.vue";
-import { jwtDecode } from 'jwt-decode';
-import { BACKEND_URL } from "@/config";
+  import axios from "axios";
+  import ModalComponent from "./ModalComponent.vue";
+  import { jwtDecode } from 'jwt-decode';
+  import { BACKEND_URL } from "@/config";
+  import { Grid } from "gridjs";
+  import "gridjs/dist/theme/mermaid.css";
 
-export default {
-  name: "GeneralCompanyList",
-  components: {
-    ModalComponent,
-  },
-  data() {
-    return {
-      companies: [],
-      modalTitle: '',
-      modalMessage: '',
-      isModalVisible: false,
-    };
-  },
-  methods: {
-    verifyLogin() {
-      // get token and verify user being admin:
-      const token = localStorage.getItem('token');
-        if (token) {
-          const decodedToken = jwtDecode(token);     
-          if (decodedToken.role === 'ADM') {
-            this.getCompanies();
+  export default {
+    name: "GeneralCompanyList",
+    components: {
+      ModalComponent,
+    },
+    data() {
+      return {
+        companies: [],
+        modalTitle: '',
+        modalMessage: '',
+        isModalVisible: false,
+      };
+    },
+    methods: {
+      verifyLogin() {
+        // get token and verify user being admin:
+        const token = localStorage.getItem('token');
+          if (token) {
+            const decodedToken = jwtDecode(token);     
+            if (decodedToken.role === 'ADM') {
+              this.getCompanies();
+            }
+            else {
+              this.modalTitle = "Acceso Restringido";
+              this.modalMessage = "Para visualizar estos datos debe haber iniciado sesión como administrador";
+              this.isModalVisible = true;
+            }
           }
           else {
             this.modalTitle = "Acceso Restringido";
             this.modalMessage = "Para visualizar estos datos debe haber iniciado sesión como administrador";
             this.isModalVisible = true;
           }
-        }
-        else {
-          this.modalTitle = "Acceso Restringido";
-          this.modalMessage = "Para visualizar estos datos debe haber iniciado sesión como administrador";
-          this.isModalVisible = true;
-        }
+      },
+      getCompanies() {
+        axios.get(`${BACKEND_URL}/Company`).then((response) => {
+          this.companies = response.data;
+          this.renderGrid();
+        });
+      },
+      renderGrid() {
+        new Grid({
+          columns: [
+            "Nombre",
+            "Identificación",
+            "Número de Teléfono",
+            "Correo",
+          ],
+          style: {
+            td: {
+              border: '1px solid #ccc',
+              'background-color': '#E3DDEC'
+            },
+            th: {
+              'font-size': '15px',
+              'background-color': '#8263a8',
+              'color': 'white'
+            },
+          },
+          data: this.companies.map((company) => [
+            company.name,
+            company.legalID,
+            company.phoneNumber,
+            company.email,
+          ]),
+          pagination: {
+            enabled: true,
+            limit: 6,
+          },
+          sort: true, 
+          search: true,
+          resizable: true, 
+          fixedHeader: true,
+          width: '100%',
+          height: '500px',
+          language: {
+            search: {
+              placeholder: "Buscar",
+            },
+            pagination: {
+              previous: "Anterior", 
+              next: "Siguiente",
+              showing: " ",
+              results: () => "",
+            },
+            sort: {
+              sortAsc: "Ordenar de forma ascendente",
+              sortDesc: "Ordenar de forma descendente",
+            },
+          }
+        }).render(this.$refs.grid);
+      },
     },
-    getCompanies() {
-      axios.get(`${BACKEND_URL}/Company`).then((response) => {
-        this.companies = response.data;
-      });
+  
+    created() {
+      this.verifyLogin();
     },
-  },
-  created() {
-    this.verifyLogin();
-  },
-};
+  };
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
 
-.container {
-  max-width: 800px;
-  margin: auto;
-  padding: 20px;
-  font-feature-settings: 'liga' off, 'clig' off;
-  font-family: Montserrat;
-}
+  .container {
+    max-width: 800px;
+    margin: auto;
+    padding: 20px;
+    font-feature-settings: 'liga' off, 'clig' off;
+    font-family: Montserrat;
+  }
 
-.row {
-  margin-bottom: 20px;
-}
+  .row {
+    margin-bottom: 20px;
+  }
 
-.go_back_button {
-  padding: 10px 15px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  font-feature-settings: 'liga' off, 'clig' off;
-  font-family: Montserrat;
-  background-color: #8263a8;
-  color: white;
-}
+  .go_back_button {
+    padding: 10px 15px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-feature-settings: 'liga' off, 'clig' off;
+    font-family: Montserrat;
+    background-color: #8263a8;
+    color: white;
+  }
 
-.title {
-  font-size: 2rem;
-  text-align: center;
-  margin-top: 10px; 
-  font-feature-settings: 'liga' off, 'clig' off;
-  font-family: Montserrat;
-}
-
-.table {
-  width: 100%;
-  border-collapse: collapse;
-  font-feature-settings: 'liga' off, 'clig' off;
-  font-family: Montserrat;
-}
-
-.table td {
-  border: 1px solid #ccc;
-  padding: 10px;
-  text-align: left;
-  background-color: #E3DDEC;
-}
-
-.table th {
-  border: 1px solid #ccc;
-  padding: 10px;
-  text-align: left;
-  background-color: #8263a8;
-  color: white;
-}
-
+  .title {
+    font-size: 2rem;
+    text-align: center;
+    margin-top: 10px; 
+    font-feature-settings: 'liga' off, 'clig' off;
+    font-family: Montserrat;
+  }
 </style>

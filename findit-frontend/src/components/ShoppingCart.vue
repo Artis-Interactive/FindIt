0243bd9e77da6e381596ea9eff3a120e568fc6c6
DariALV <darialvjs@gmail.com>
2024-10-24 08:46:29 -0600

    Shopping Cart Body and Title

---
+++ findit-frontend/src/components/ShoppingCart.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <AppHeader></AppHeader>
+    <div class="cartTitle"><i class="uil uil-shopping-cart"></i><h1>Mi carrito</h1></div>
+    <div class="cartBody"></div>
+  </div>
+</template>
+
+<script>
+  // import axios from 'axios';
+  import AppHeader from './AppHeader.vue';
+  // import { BACKEND_URL } from "@/config";
+  export default {
+    name: 'ProductCreation',
+        components: {
+            AppHeader
+        }, 
+        data() {
+            return {
+                products: [
+                  {name: "Test", img: "", basePrice: 500, cartQuantity: 5, companyName: "TestComp", productionDays: ["Lunes", "Sabado"]},
+                  {name: "TP", img: "", basePrice: 45600, cartQuantity: 65, companyName: "Comp", productionDays: ["Miercoles"]},
+                  {name: "Marco", img: "", basePrice: 1, cartQuantity: 777, companyName: "Marquillos", productionDays: ["Miercoles", "Sabado", "Domingo"]}
+                ]
+            }
+        },
+        methods: {
+        handleProductsData() {}
+        },
+        created() {}
+  }
+</script>
+
+<style scoped>
+
+</style>

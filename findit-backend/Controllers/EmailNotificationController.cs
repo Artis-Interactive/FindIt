2a7721c7dac6d204986b63c8d9e3a8ae466350f2
Sebastián Bolaños <sebasbose@gmail.com>
2024-10-24 16:29:58 -0600

    Added hostedUrl parameter to NewOrderEmailNotification endpoint.

--- findit-backend/Controllers/EmailNotificationController.cs
+++ findit-backend/Controllers/EmailNotificationController.cs
@@ -35,7 +35,7 @@ public class EmailNotificationController : ControllerBase
     }
 
     [HttpPost("sendNewOrderEmail")]
-    public async Task<IActionResult> SendNewOrderEmail()
+    public async Task<IActionResult> SendNewOrderEmail(string hostedUrl)
     {
         if (!ModelState.IsValid)
         {
@@ -50,7 +50,7 @@ public class EmailNotificationController : ControllerBase
                 <p style='color: #555555; font-size: 16px; line-height: 1.5;'>¡Se ha recibido un nuevo pedido! Haz click en el boton a continuación para aprobarlo o rechazarlo.</p>
                 <center>
             	    <p>
-                      <a href='[SURVEYURL]' style='border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; background-color: #8263A8;'>Aprobar o rechazar pedido</a>
+                      <a href='{hostedUrl}' style='border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; background-color: #8263A8;'>Aprobar o rechazar pedido</a>
                     </p>
                 </center>";
 

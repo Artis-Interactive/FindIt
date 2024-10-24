319f43965e36a69d3d3b1d4dfa2a341552bae8c6
Sebastián Bolaños <sebasbose@gmail.com>
2024-10-23 21:50:48 -0600

    Now the HOSTED_URL is obtained dynamically.

--- findit-frontend/src/config.js
+++ findit-frontend/src/config.js
@@ -1,2 +1,2 @@
 export const BACKEND_URL = 'https://localhost:7262/api';
-export const HOSTED_URL = 'http://localhost:8080'; // Cambiar por url de alojamiento del webapp
+export const HOSTED_URL = new URL(location.href)['origin'];
